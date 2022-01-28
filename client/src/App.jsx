397c91e932fa3b51efaee53318c586917db7a10d
Derek Mason <3derekmason@gmail.com>
2022-01-28 15:08:25 -0700

    get text on app.jsx

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import React, { useState, useEffect, useRef } from "react";
+
+const App = () => {
+  return (
+    <div>
+      <h1>hello world</h1>
+      <p>by Derek</p>
+    </div>
+  );
+};
+
+export default App;

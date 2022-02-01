447ab0214d412d4d0021f37b73d3a1b509c76b87
Derek Mason <3derekmason@gmail.com>
2022-02-01 08:39:50 -0700

    home page complete

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -20,8 +20,30 @@ const Home = () => {
           <div className={toggleClass} onClick={toggleMenu}></div>
         </header>
         <img src={treeBackground}></img>
+        <ul className="social">
+          <li style={{ transitionDelay: "0.2s" }}>
+            <a href="#">LinkedIn</a>
+          </li>
+          <li style={{ transitionDelay: "0.2s" }}>
+            <a href="#">Github</a>
+          </li>
+          <li style={{ transitionDelay: "0.2s" }}>
+            <a href="#">Twitter</a>
+          </li>
+        </ul>
+        <ul className="navigation">
+          <li style={{ transitionDelay: "0.2s" }}>
+            <a href="#">Home</a>
+          </li>
+          <li style={{ transitionDelay: "0.4s" }}>
+            <a href="#">About Me</a>
+          </li>
+          <li style={{ transitionDelay: "0.6s" }}>
+            <a href="#">Projects</a>
+          </li>
+        </ul>
         <div className="glass"></div>
-        <div class="content">
+        <div className="content">
           <h2>
             Hi there! <br />
             <span>My name is Derek.</span>

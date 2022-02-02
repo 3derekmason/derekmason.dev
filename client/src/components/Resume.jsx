4c82c232e9a1c611c09ef4581a8d1e41dee8437e
Derek Mason <3derekmason@gmail.com>
2022-02-02 11:57:30 -0700

    resume header

--- client/src/components/Resume.jsx
+++ client/src/components/Resume.jsx
@@ -1,5 +1,9 @@
 import React, { useState, useContext } from "react";
 import treeBackground from "../../public/assets/trees.jpg";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import GitHubIcon from "@mui/icons-material/GitHub";
+
+import { Typography } from "@mui/material";
 
 import AppContext from "../context.js";
 import Home from "./Home.jsx";
@@ -25,6 +29,42 @@ const Resume = () => {
         </header>
         <img src={treeBackground} />
         <div className="glass"></div>
+        <div className="resumeContent">
+          <div className="resumeHeader">
+            <Typography element="h3" variant="h4">
+              Derek Mason
+            </Typography>
+            <Typography element="h4" variant="subtitle1">
+              Denver, CO |{" "}
+              <a href="mailto:3derekmason@gmail.com">3derekmason@gmail.com</a> |{" "}
+              <a href="tel:970-551-9044">970-551-9044</a>
+            </Typography>
+            <Typography
+              style={{
+                width: "100%",
+                display: "flex",
+                justifyContent: "center",
+                gap: "32px",
+                marginTop: "4px",
+              }}
+            >
+              <a
+                href="https://www.linkedin.com/in/3derekmason/"
+                target="_blank"
+              >
+                <LinkedInIcon />
+              </a>
+              <a href="https://www.github.com/3derekmason" target="_blank">
+                <GitHubIcon />
+              </a>
+            </Typography>
+          </div>
+          <div className="techSkills"></div>
+          <div className="softwareEngineering"></div>
+          <div className="experience"></div>
+          <div className="education"></div>
+          <div className="resumeFooter"></div>
+        </div>
       </section>
     </div>
   );

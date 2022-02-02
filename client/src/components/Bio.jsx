230698a15b63b7bbd0fd618973e118d7f8237e00
Derek Mason <3derekmason@gmail.com>
2022-02-01 18:21:48 -0700

    styling touchups

--- client/src/components/Bio.jsx
+++ client/src/components/Bio.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useContext } from "react";
 import treeBackground from "../../public/assets/trees.jpg";
-
+import { LinkedInIcon, GitHubIcon, TwitterIcon } from "@mui/icons-material";
 import AppContext from "../context.js";
 import Home from "./Home.jsx";
 
@@ -35,34 +35,35 @@ const AboutMe = () => {
           </div>
           <div className="bioText">
             <p>
-              Hello! As you saw on the landing page, my name is Derek Mason. I
-              am a colorado native with a passion for problem solving and a
-              strong desire to see everyone around me succeed.
+              <strong>Hello!</strong> As you saw on the landing page, my name is
+              Derek Mason. I am a colorado native with a passion for problem
+              solving and a strong desire to see everyone around me succeed.
             </p>
             <p>
-              Originally my passion of helping and teaching others led me to be
-              a preschool teacher. I love helping to create lifelong learners,
-              one reason software development began to call to me is because I
-              am a lifelong learner myself! Another passion of mine, fitness and
-              health, convinced me to get certified as a Personal Trainer on the
-              side. Now I was helping others learn (and improve quality of life)
-              at all ages.
+              <strong>Originally my passion</strong> of helping and teaching
+              others led me to be a preschool teacher. I love helping to create
+              lifelong learners, one reason software development began to call
+              to me is because I am a lifelong learner myself! Another passion
+              of mine, fitness and health, convinced me to get certified as a
+              Personal Trainer on the side. Now I was helping others learn (and
+              improve quality of life) at all ages.
             </p>
             <p>
-              Then in 2020, my adventure into software development really took
-              off. After months of not being able to work due to COVID-19, I
-              realized that I needed to work in an industry that couldn't be as
-              disrupted by world events. A friend of mine turned me to Galvanize
-              / Hack Reactor and the rest was history... I quickly fell in love
-              with Javascript and the logical brain puzzles and satisfaction
-              upon thier completion. I continued to learn more and graduated
-              from Hack Reactor's Software Engineering Immersive in December
-              2021. Realizing that there is nothing I couldn't do (and that I
-              would never run out of things to stop learning) really solidified
-              in my mind that I was really meant to be a developer.
+              <strong>Then in 2020,</strong> my adventure into software
+              development really took off. After months of not being able to
+              work due to COVID-19, I realized that I needed to work in an
+              industry that couldn't be as disrupted by world events. A friend
+              of mine turned me to Galvanize / Hack Reactor and the rest was
+              history... I quickly fell in love with Javascript and the logical
+              brain puzzles and satisfaction upon thier completion. I continued
+              to learn more and graduated from Hack Reactor's Software
+              Engineering Immersive in December 2021. Realizing that there is
+              nothing I couldn't do (and that I would never run out of things to
+              stop learning) really solidified in my mind that I was really
+              meant to be a developer.
             </p>
             <p>
-              Currently, <strong>MERN</strong> (
+              <strong>Currently, MERN</strong> (
               <em>MongoDB, Express, React, NodeJS</em>) is probably my favorite
               tech stack to play with, however I have an equal love for the
               front and back end of development.
@@ -72,6 +73,11 @@ const AboutMe = () => {
               <a href="mailto:3derekmason@gmail.com">Shoot me a message</a> if
               you'd like to connect.
             </p>
+          </div>
+          <div className="socialBtns">
+            <LinkedInIcon />
+            <GitHubIcon />
+            <TwitterIcon />
           </div>
         </div>
       </section>

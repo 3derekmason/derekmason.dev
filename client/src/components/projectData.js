318bb4aea2793abfe3103b9ef175da2b5e404c23
Derek Mason <3derekmason@gmail.com>
2022-02-02 17:45:23 -0700

    add project summaries to data

--- client/src/components/projectData.js
+++ client/src/components/projectData.js
@@ -13,7 +13,8 @@ const projectData = [
     imgURL: "./assets/stemvpss.png",
     subtitle:
       "A full scale application for ECE Instructors made in a two day timebox",
-    summary: "",
+    summary:
+      "A stem lab activity app for ECE teachers. Display all activities or a single random activity. There is also the option to add a new activity, sort activities by category, or select a single random activity from all available.",
     deployed: "https://stemvp.herokuapp.com/",
     code: "https://github.com/3derekmason/steMVP",
   },
@@ -21,15 +22,17 @@ const projectData = [
     title: "Css Pizazz",
     imgURL: "./assets/csspss.png",
     subtitle: "My playground for css animations",
-    summary: "",
+    summary:
+      "Just a little portfolio made with Vue3 to show off some animations I've been playing around with!",
     deployed: "https://csspizazz.herokuapp.com/",
     code: "https://github.com/3derekmason/css-pzazz",
   },
   {
     title: "Help Me Out",
     imgURL: "",
-    subtitle: "Full scale production of a community centered handyman app",
-    summary: "",
+    subtitle: "Find or offer help in your community",
+    summary:
+      "HelpMeOut is a job board application created to connect members of the community in a safe, accessible manner. An idea presented by a client, and implemented by a team of developers aiming to provide a premium product that met proposed criteria.",
     deployed: "",
     code: "https://github.com/3derekmason/HelpMeOut",
   },
@@ -37,7 +40,8 @@ const projectData = [
     title: "Project Island Walk",
     imgURL: "./assets/islandss.png",
     subtitle: "UX/UI modernization of a retail website + full API design",
-    summary: "",
+    summary:
+      "A project with the mission to modernize an out-of-date web application. Rebranding of an e-commerce website with Overview, Related Items, Custom Outfit, and Ratings and Reviews components for each product.",
     deployed: "",
     code: "https://github.com/3derekmason/Project-IslandWalk",
   },
@@ -45,7 +49,8 @@ const projectData = [
     title: "Vue View",
     imgURL: "",
     subtitle: "An app to share your great views easily with vue",
-    summary: "",
+    summary:
+      "Have you ever wanted to post an image online, but felt hesitant because of the toxicity of social media? Have you ever wanted to design an app using VueJS? VueView is simply an app to share your great views easily with vue. ",
     deployed: "",
     code: "https://github.com/PopSmartInc/VueView",
   },

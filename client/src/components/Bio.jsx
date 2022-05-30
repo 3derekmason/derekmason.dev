5e267c6b08aa3923c4c59a87fad07107e8af0203
Derek Mason <3derekmason@gmail.com>
2022-05-30 09:50:01 -0600

    update bio

--- client/src/components/Bio.jsx
+++ client/src/components/Bio.jsx
@@ -39,55 +39,48 @@ const AboutMe = () => {
             <p>
               <strong style={{ letterSpacing: "1.5px" }}>Hello!</strong> As you
               saw on the landing page, <strong>my name is Derek Mason</strong>.
-              I am a Colorado native with a passion for problem solving and a
-              strong desire to see those around me solve thier problems as well.
+              I love seeing people succeed. I'm very passionate about making a
+              positive impact on my community and the lives I interact with.
             </p>
             <br />
             <p>
               <strong style={{ letterSpacing: "1.5px" }}>
                 Originally, my love
               </strong>{" "}
-              of helping and teaching others led me to be a preschool teacher.
-              My joy in creating lifelong learners is one reason that software
-              development began to call to me, because I am a lifelong learner
-              myself! Another passion of mine, fitness and health, convinced me
-              to also get certified as a Personal Trainer. I was able to help
-              others learn and improve quality of life at all ages. It is very
-              important to me that what I do will benefit my community.
+              of helping and teaching others led me to pursue a career as a
+              preschool teacher. At the same time, another passion of mine,
+              fitness and health, convinced me to also get certified as a
+              Personal Trainer. I was able to help others learn and improve
+              quality of life at all ages...
             </p>
             <br />
             <p>
-              <strong style={{ letterSpacing: "1.5px" }}>In 2020,</strong> my
-              adventure into software development really took off... After
-              months of not being able to work due to COVID-19, I realized that
-              I wanted and needed to be in an industry that couldn't be as
-              disrupted by world events. A friend of mine turned me to
-              Galvanize/Hack Reactor and I began researching and took time to
-              learn some of the Javascript basics. I quickly fell in love with
-              the puzzles I was experiencing (and of course the satisfaction
-              upon their completion). I continued to learn more and pushed
-              myself through months of 12 hour coding days. I built Full Stack
-              applications with a some awesome teams, eventually leading to my
-              graduation of Hack Reactor's Software Engineering Immersive in
-              December 2021.
+              <strong style={{ letterSpacing: "1.5px" }}>In 2020,</strong> in
+              part due to being stuck at home from COVID-19, I started to dip my
+              toes into web development. I quickly fell in love with the problem
+              solving and algorithms, and enrolled in a reccommended software
+              engineering bootcamp as soon as I could. About 3 and a half months
+              after diving off the deep end (of JavaScript), I graduated from
+              Hack Reactor's Advanced Full Stack Software Engineering Immersive.
+            </p>
+            <p>
+              Since then I've been learning every day, constantly falling in
+              love with new languages and technologies. I make the joke with my
+              friends that with web development I will never be bored again!
+              There is always something exciting or interesting to dig into.
             </p>
             <br />
             <p>
               <strong style={{ letterSpacing: "1.5px" }}>Currently,</strong> I'm
               living in the Denver area. When I'm not coding, I will most likely
               be at the gym, somewhere outside in the sunshine, or watching a
-              76ers game, <strong>Trust the Process</strong>! My favorite tech
-              stack to play with right now is probably MERN,{" "}
-              <em>MongoDB, ExpressJS, React, NodeJS</em>. I love creating pages
-              that are accessible, stimulating, and satisfying for the user. On
-              the flip side, I thoroughly enjoy building organized and high
-              performance APIs on the back end! Tell me about an exciting new
-              technology and I will most likely soon be experimenting with it
-              myself.
+              76ers game! I love prioritizing accessibility when creating pages.
+              My goal is to create beautiful applications that are mindfull of
+              every potential user.
             </p>
             <br />
             <p>
-              I'm also currently looking for new opportunities! If you'd like to
+              Right now I am looking for new opportunities! If you'd like to
               connect with me,{" "}
               <a href="mailto:3derekmason@gmail.com">shoot me an email</a> or
               check out any of my social profiles below.
